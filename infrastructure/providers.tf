8f1e167b4fe8d1aeab44c6623b23386d0847e2bb
Rofiat <oyizakareem9@gmail.com>
2025-06-23 16:56:32 +0100

    added redshift task

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
